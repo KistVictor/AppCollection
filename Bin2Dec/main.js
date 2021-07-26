0b25712d1189513c94e147338d45937131a83683
Victor Kist <victoraugustokist@gmail.com>
2021-07-26 11:07:00 -0300

    ajuste organização

---
+++ Bin2Dec/main.js
@@ -0,0 +1,43 @@
+  const bin2Dec = (event) => {
+    event.preventDefault()
+
+    bin = document.querySelector("[data-form-input]").value
+
+    const isOnly01 = verifyNumbers(bin)
+    
+    if (!isOnly01) {
+      return
+    }
+
+    dec = parseInt(bin, 2)
+
+    showDecimalNumber(dec)
+
+  }
+
+  function verifyNumbers(bin) {
+    const binArray = bin.split('') 
+
+    for(let i = 0; i < binArray.length; i++) {
+      if(binArray[i] != 0 && binArray[i] != 1) {
+        window.alert("Only 0 and 1 accepted")
+        return false
+      }
+    }
+    
+    return true
+  }
+
+  function showDecimalNumber(dec) {
+    const form = document.querySelector("form")
+
+    const decimalNumber = document.createElement("p")
+
+    decimalNumber.innerText = dec
+
+    form.appendChild(decimalNumber)
+  }
+
+  const formButton = document.querySelector("[data-form-button]");
+  
+  formButton.addEventListener("click", bin2Dec);
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
  # Ref
  - https://github.com/florinpop17/app-ideas
- ## Scripts inside HTML
 
  sass ./*project*/styles/index.scss ./*project*/styles/index.css
