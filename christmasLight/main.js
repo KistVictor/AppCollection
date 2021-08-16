758a7411f5af01287992a6d634356ad5148d5496
Victor Kist <victoraugustokist@gmail.com>
2021-08-16 13:48:02 -0300

    add function addLight

--- christmasLight/main.js
+++ christmasLight/main.js
@@ -1,15 +1,11 @@
 const lights = document.querySelectorAll(".light")
 const speedValue = document.querySelector(".speedValue")
-const lightQtdValue = document.querySelector(".lightQtdValue")
+const lightsContent = document.querySelector("header")
 
 const setSpeed = () => {
   for (let i = 0; i < lights.length; i++) {
     lights[i].style.animationDuration = speedValue.value + 's'
   }
-}
-
-const setLightQtd = () => {
-  console.log(lightQtdValue.value)
 }
 
 const on = () => {
@@ -24,4 +20,15 @@ const off = () => {
     lights[i].style.animation = "none"
     lights[i].style.background = "#666666"
   }
+}
+
+const addLight = () => {
+  const newLight = document.createElement("div")
+  newLight.append("teste")
+  lightsContent.parentNode.insertBefore(newLight, lightsContent)
+  console.log(lightsContent)
+}
+
+const removeLight = () => {
+
 }
